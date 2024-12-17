7584146a45152ddb6b0027a228c36674ed7e3d14
Balram <balram.ak@bitcot.com>
2024-12-17 16:35:46 +0530

    Initial commit

---
+++ Dawn/Main.swift
@@ -0,0 +1,17 @@
+//
+//  Main.swift
+//  Dawn
+//
+//  Created by bitcot on 17/12/24.
+//
+
+import Foundation
+
+public class Testing{
+    public init() {}
+    
+    public func printMessage(mesaage:String) {
+        print(mesaage)
+    }
+    
+}
